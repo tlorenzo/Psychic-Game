43ec5fad211207895a25438c43c11f35328281d2
Tony Isidro <tonyi@Tonys-MBP.fios-router.home>
2018-03-18 13:41:00 -0400

    Updated files

---
+++ javascript/game.js
@@ -0,0 +1,78 @@
+var alphabetSoup = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+
+var wins = 0;
+var losses = 0;
+var guessesLeft = 10;
+var yourGuessesSoFar = [];
+var gamesPlayed = 0;
+var humanChoice;
+var appChoice;
+var letterLottery = alphabetSoup[Math.floor(Math.random() * alphabetSoup.length)];
+console.log(letterLottery);
+
+
+function lottery(){
+var letterLottery = alphabetSoup[Math.floor(Math.random() * alphabetSoup.length)];
+console.log(letterLottery);
+}
+
+
+
+//Inner HTML Stuffs on Start Up
+document.getElementById("wins").innerHTML = "Wins: " + wins;
+document.getElementById("losses").innerHTML = "Losses: " + losses;
+document.getElementById("guessesLeft").innerHTML = "Guesses Left: " + guessesLeft;
+document.getElementById("yourGuessesSoFar").innerHTML = "Guesses So Far: " + yourGuessesSoFar;
+
+document.onkeyup = function (event) {
+    humanChoice = String.fromCharCode(event.which).toLowerCase();
+    //console.log(humanChoice);
+    guessesLeft--;
+    //console.log(guessesLeft);
+    document.getElementById("message").innerHTML = "Keep going.  Choose another letter.";
+    document.getElementById("guessesLeft").innerHTML = "Guesses Left: " + guessesLeft;
+   
+    if (guessesLeft > 0 && alphabetSoup.indexOf(humanChoice) >= 0 ) {
+        yourGuessesSoFar.push(" " + humanChoice);
+        //console.log(yourGuessesSoFar);
+        document.getElementById("yourGuessesSoFar").innerHTML = "Guesses So Far: " + yourGuessesSoFar;
+
+    }
+  
+
+
+
+    if (humanChoice == letterLottery) {
+        wins++;
+        gamesPlayed++;
+        document.getElementById("message").innerHTML = "Yes! I was thinking of " + letterLottery + "! <br>You have guessed correctly! <br>Press any letter to play another game.";
+        letterLottery = alphabetSoup[Math.floor(Math.random() * alphabetSoup.length)];
+        guessesLeft = 10;
+        yourGuessesSoFar = [];
+        document.getElementById("gamesPlayed").innerHTML = "Total Games Played: " + gamesPlayed;
+        document.getElementById("wins").innerHTML = "Wins: " + wins;
+        document.getElementById("losses").innerHTML = "Losses: " + losses;
+        document.getElementById("guessesLeft").innerHTML = "Guesses Left: " + guessesLeft;
+        document.getElementById("yourGuessesSoFar").innerHTML = "Guesses So Far: " + yourGuessesSoFar;
+        
+
+    }
+
+
+
+    if (guessesLeft == 0) {
+        losses++;
+        gamesPlayed++;
+        guessesLeft = 10;
+        yourGuessesSoFar = []; 
+        lottery();
+        document.getElementById("message").innerHTML = "You have run out of guesses!  Press any letter to try again.";
+        document.getElementById("gamesPlayed").innerHTML = "Total Games Played: " + gamesPlayed;
+        document.getElementById("wins").innerHTML = "Wins: " + wins;
+        document.getElementById("losses").innerHTML = "Losses: " + losses;
+        document.getElementById("guessesLeft").innerHTML = "Guesses Left: " + guessesLeft;
+        document.getElementById("yourGuessesSoFar").innerHTML = "Guesses So Far : " + yourGuessesSoFar;
+        
+    }
+
+}
